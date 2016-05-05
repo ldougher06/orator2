127c81d3fbff9180c068f25daf08fcc3892cbaaa
logan dougher <logan.dougher@gmail.com>
2016-05-04 19:04:01 -0500

    started building client side

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,28 @@
 'use strict'
 
 console.log(" *** IN MAIN.JS *** ")
+
+const app = angular.module('oratorTwo', []);
+
+app.controller('MainCtrl', [
+  '$scope', ($scope) => {
+    $scope.posts = [
+      {name: 'John', title: 'Junior Developer'},
+      {name: 'Tina', title: 'Mid-Level Developer'},
+      {name: 'Trish', title: 'Senior Developer'},
+      {name: 'Bruno', title: 'Smarty'},
+      {name: 'Tommy', title: 'Likes 2 Party'}
+    ];
+
+    $scope.addPost = () => {
+      $scope.posts.push(
+        {
+          name: $scope.name,
+          title: $scope.title,
+          link: $scope.link
+        })
+      $scope.name = '';
+      $scope.title = '';
+      $scope.link = '';
+    }
+}]);
